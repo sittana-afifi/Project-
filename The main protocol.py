fd4fcbf1b93e9b4de920422ef417bd471c29fc6a
Sittana Afifi <64017181+sittana-afifi@users.noreply.github.com>
2020-05-07 03:11:17 +0200

    Update The main protocol.py

--- The main protocol.py
+++ The main protocol.py
@@ -27,26 +27,26 @@ def test_isomorphism():
 import math
 
 
+import math
 def honest_prover(AM,AM1,pi,mess_list,s):
     n=len(AM)
     if len(mess_list)==0:
         np.random.seed(s)
         sigma =np.random.permutation(n)
-        h=apply_permut(AM,sigma)
-        H=generate_graph(h)
-        mess_list.append(H)
-        #print(sigma)
+        h=apply_permute(AM,sigma)
+        mess_list.append(h)
     if len(mess_list)==2:
         np.random.seed(s)
         sigma =np.random.permutation(n)
         ch=mess_list[1]
-        #print(sigma)
         if ch==0:
             mess_list.append(sigma)
         else:
             p=inv(pi)
-            mess_list.append(composite_permutition(sigma,p))
+            mess_list.append(composite_permutition(p,list(sigma)))
     return mess_list
+
+
 
 def honest_verifier(AM,AM1,mess_list):
     if len(mess_list)==1:
@@ -54,23 +54,22 @@ def honest_verifier(AM,AM1,mess_list):
         mess_list.append(ch)
     if len(mess_list)==3: 
         phi=mess_list[2]
-        H=Graph(directed=False)
+        ch=mess_list[1]
         H=mess_list[0]
-        H0=apply_permut(AM,phi)
-        H1=generate_graph(H0)
-        print(g0,g1)
-        if are_same(H,H1):
+        mess_list[0]=generate_graph(H)
+        if ch==0 :
+            H0=apply_permute(AM,phi)
+        if ch==1:
+            H0=apply_permute(AM1,phi)
+        if are_equal(H,H0):
             mess_list.append('accept')
         else:
             mess_list.append('reject')
-        
     return mess_list
     
     
 def test_isomorphism():
-    AM=enter_adjMatrix()
-    AM1=enter_adjMatrix()
-    pi=list(map(int, input('Enter pi:').split())) 
+    s=random.choice(range(1000))
     V=lambda msg: honest_verifier(AM,AM1,msg)
     P=lambda msg: honest_prover(AM,AM1,pi,msg,s)
     return graph_isomorphism(P,V)
@@ -84,6 +83,44 @@ def protocol_dishonest_prover(G0, G1):
     P=lambda msg: cheating_prover(AM,AM1,msg,s)
     return graph_isomorphism(P,V)
     
+    
+    
+import csv
+def get_graph_from_file(i):
+    with open('matices.csv') as f:
+        content = f.readlines()
+    content = [x.strip() for x in content] 
+    l=[]
+    l=content[i]
+    matrix=[]
+    l[i]
+    n=int(np.sqrt(len(l)-2))
+    for i in range(1,len(l)-1):
+        matrix.append(l[i])
+    s=np.reshape(matrix,(n,n))
+    return s
+
+def get_pi_from_file(i):
+    with open('matices.csv') as f:
+        content = f.readlines()
+    content = [x.strip() for x in content] 
+    l=[]
+    l=content[i]
+    matrix=[]
+    l[i]
+    n=int(np.sqrt(len(l)-2))
+    for i in range(1,len(l)-1):
+        matrix.append(int(l[i]))
+    return matrix
+
+def equal(AM):
+    a=np.zeros([4,4])
+    for i in range(4):
+        for j in range(4):
+            a[i][j]=AM[i,j]
+    return a
+
+
     
  def cheating_prover(AM, AM1, mess,s):
     n=len(AM)
